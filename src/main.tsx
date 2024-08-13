36b4ce5ba8a2bc9f7fbd70c3a0d7dc25ce0df311
eftsoons <svetafedorowitch@yandex.ru>
2024-08-13 21:03:09 +0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import ReactDOM from "react-dom/client";
+import { AppRoot } from "@telegram-apps/telegram-ui";
 import App from "./App.tsx";
 
 import "@telegram-apps/telegram-ui/dist/styles.css";
 import "./scss/main.scss";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <AppRoot>
+    <App />
+  </AppRoot>
+);
